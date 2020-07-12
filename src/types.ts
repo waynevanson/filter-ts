808f502721f25c4e2d8f59d2eff19f8e31d7cfb2
Wayne Van Son <waynevanson@gmail.com>
2020-07-13 00:25:12 +1000

    chore: type defs for navigating refinement params and return values

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import { UnionizeArray } from "./utils";
+import { UnionToIntersection } from "./utils/types";
+
 export interface Predicate<A> {
   (a: A): boolean;
 }
@@ -9,3 +12,43 @@ export interface Refinement<A, B extends A> {
 export interface Definement<T, U> {
   <A extends T>(a: A): a is A extends U ? A : never;
 }
+
+// PARAMS
+
+export type RefinementUnionParameter<
+  D extends readonly Refinement<any, any>[]
+> = UnionizeArray<D> extends infer U
+  ? U extends any
+    ? U extends Refinement<infer A, any>
+      ? A
+      : never
+    : never
+  : never;
+
+export type RefinementIntersectParameter<
+  D extends readonly Refinement<any, any>[]
+> = UnionToIntersection<RefinementUnionParameter<D>>;
+
+// RETURNS
+
+export type RefinementUnionReturn<
+  D extends readonly Refinement<any, any>[]
+> = UnionizeArray<D> extends infer U
+  ? U extends any
+    ? U extends Refinement<any, infer B>
+      ? B
+      : never
+    : never
+  : never;
+
+export type RefinementIntersectReturn<
+  D extends readonly Refinement<any, any>[]
+> = UnionToIntersection<RefinementUnionReturn<D>>;
+
+// MATCH
+
+export type RefinementMatchParam<
+  D extends readonly Refinement<any, any>[],
+  A extends RefinementIntersectParameter<D> = RefinementIntersectParameter<D>,
+  B extends RefinementUnionParameter<D> = RefinementUnionParameter<D>
+> = A extends B ? (B extends A ? A : never) : never;
