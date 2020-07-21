182708867c7eb269fa492675f027d81a70bbaa8b
Wayne Van Son <waynevanson@gmail.com>
2020-07-21 12:58:56 +1000

    refactor: remove unused types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,18 +1,3 @@
-export type UnionToIntersection<U> = (
-  U extends any ? (k: U) => void : never
-) extends (k: infer I) => void
-  ? I
-  : never;
-
-export type EnforceTupleArray<T extends readonly any[]> = T extends {
-  0: any;
-  1: any;
-}
-  ? T
-  : never;
-
-export type UnionizeArray<T> = T extends Array<infer U> ? U : never;
-
 export type And<A, B> = A & B;
 export type Or<A, B> = A | B;
 export type Not<A, B> = Exclude<A, B>;
